5ac8bcad68a3501d72c358de69b1b93095f9bdf4
William Rhodes <frhodes364@gmail.com>
2024-02-16 00:42:00 -0500

    Add project files.

---
+++ GriefPrevention/Events/BarricadeDamaging.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenMod.API.Eventing;
+using OpenMod.API.Permissions;
+using OpenMod.API.Persistence;
+using OpenMod.API.Users;
+using OpenMod.Unturned.Building.Events;
+using OpenMod.Unturned.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GriefPrevention.Events
+{
+    public class BarricadeDamaging : IEventListener<UnturnedBarricadeDamagingEvent>
+    {
+        private readonly IConfiguration m_Configuration;
+        private readonly bool m_enable_grief_prevention;
+
+        public BarricadeDamaging(IConfiguration configuration)
+        {
+            m_Configuration = configuration;
+
+            m_enable_grief_prevention = m_Configuration.GetValue<bool>("enable_grief_prevention");
+        }
+
+        public async Task HandleEventAsync(object sender, UnturnedBarricadeDamagingEvent @event)
+        {
+            if (m_enable_grief_prevention)
+            {
+                @event.IsCancelled = true;
+            }
+        }
+    }
+}
