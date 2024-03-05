ce44366c2ec87d540728feb20eed2d331b56a974
William Rhodes <frhodes364@gmail.com>
2024-03-04 21:38:15 -0500

    Fixed plant not breaking when harvested

--- GriefPrevention/Events/BuildableDamaging.cs
+++ GriefPrevention/Events/BuildableDamaging.cs
@@ -3,8 +3,11 @@ using OpenMod.API.Eventing;
 using OpenMod.API.Permissions;
 using OpenMod.API.Persistence;
 using OpenMod.API.Users;
+using OpenMod.Core.Users;
+using OpenMod.Unturned.Building;
 using OpenMod.Unturned.Building.Events;
 using OpenMod.Unturned.Users;
+using SDG.Unturned;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -16,19 +19,28 @@ namespace GriefPrevention.Events
     {
         private readonly IConfiguration m_Configuration;
         private readonly bool m_enable_grief_prevention;
+        private readonly UnturnedUserDirectory m_UnturnedUserDirectory;
 
-        public BuildableDamaging(IConfiguration configuration)
+        public BuildableDamaging(IConfiguration configuration, UnturnedUserDirectory unturnedUserDirectory)
         {
             m_Configuration = configuration;
 
             m_enable_grief_prevention = m_Configuration.GetValue<bool>("enable_grief_prevention");
+            m_UnturnedUserDirectory = unturnedUserDirectory;
         }
 
         public async Task HandleEventAsync(object sender, UnturnedBuildableDamagingEvent @event)
         {
             if (m_enable_grief_prevention)
             {
-                @event.IsCancelled = true;
+                if (@event.Buildable is UnturnedBarricadeBuildable barricadeBuildable && barricadeBuildable.BarricadeDrop.interactable is InteractableFarm plant)
+                {
+                    return;
+                }
+                else
+                {
+                    @event.IsCancelled = true;
+                }
             }
         }
     }
